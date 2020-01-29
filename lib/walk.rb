d70d4825f298720547c10634cbfc634e2dc2017f
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-29 12:08:19 +0000

    challenge complete, test passes

--- lib/walk.rb
+++ lib/walk.rb
@@ -1,11 +1,27 @@
 class Walk
-
+    attr_reader :array
+    def initialize(array = nil)
+        @array = array 
+    end 
     
     def create_array
+        arr = []
         10.times do 
-            arr = []
             arr << ['n', 's', 'e', 'w'].sample
-            arr
-        end 
+        end
+        @array = arr
     end 
+
+    def return_position(array)
+        x = 5
+        y = 5
+        array.each do |l|
+            if l == "n" then x += 1
+            elsif l == "s" then x -= 1
+            elsif l == "e" then y += 1
+            elsif l == "w" then y -= 1
+            end
+        end
+         x == 5 && y == 5
+    end
 end 
--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -1,16 +1,30 @@
 require 'walk'
 
 describe Walk do 
-    let(:array) { Walk.new.create_array }
+    let(:walk) { Walk.new() }
 
     it 'Should respond to create_array method' do
-        walk = Walk.new
         expect(walk).to respond_to(:create_array)
     end 
 
-    it "Should give a random array from 4 arrays given" do 
-        expect(array.count).to eq(10)
+    it "Should give a random array of 10 characters" do 
+        expect((walk.create_array).count).to eq(10)
+
     end 
+    describe '#return_position' do 
+        it 'should return true if it array of directions return to same position' do 
+            array = walk.create_array
+            expect(walk.return_position(array)).to eq(true).or eq(false)
+        end 
 
+        it 'should return true or the following test' do 
+            walk = Walk.new(array = ['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])
+            expect(walk.return_position(array)).to eq(true)
+        end 
 
+        it 'should return false to the following test' do 
+            walk = Walk.new(array = ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])
+            expect(walk.return_position(array)).to eq(false)
+        end 
+    end 
 end 
